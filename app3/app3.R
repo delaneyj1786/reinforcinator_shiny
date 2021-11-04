72f4bccdb9fa1d850f5d61c5951b3d60342b80e2
James <delaneyj@pdx.edu>
2021-11-04 14:03:23 -0700

    something weird was happening ...

--- app3/app3.R
+++ app3/app3.R
@@ -1,6 +1,7 @@
 library(shiny)
 library(devtools)
-install_github("delaneyj1786/REINFORCINATOR")
+#install_github("delaneyj1786/REINFORCINATOR")
+library(ReenforcinateR)
 library(tidyverse)
 
 ## Needs a pipeline
