c7610adfe691ad06e1733d3a1884289a19e95220
James <delaneyj@pdx.edu>
2021-11-22 15:09:03 -0800

    https://stackoverflow.com/questions/22206290/r-shiny-making-sub-panels updated this way ... not working

--- Layout_testing/layout_testing.R
+++ Layout_testing/layout_testing.R
@@ -7,31 +7,29 @@ ui <- fluidPage(
     # Application title
     titlePanel("Old Faithful Geyser Data"),
 
-    # Sidebar with a slider input for number of bins
-    sidebarLayout(
-        sidebarPanel(
-            radioButtons("controller", "Controller", 1:3,1)
-        ),
-
         # Show a plot of the generated distribution
-        mainPanel(
             tabsetPanel(
-                id = "hidden_tabs",
-                type = "hidden",
-                tabPanelBody("panel1", "Panel 1 Contents"),
-                tabPanelBody("panel2", "Panel 2 Contents"),
-                tabPanelBody("panel3", "Panel 3 Contents")
+                id = "selectedtab",
+                tabPanel("Tab1", uiOutput("Tab1"),
+                tabPanel("Tab2", uiOutput("Tab2"))
+                )
             )
         )
-    )
-)
+
 
 # Define server logic required to draw a histogram
 server <- function(input, output, session) {
-observeEvent(input$controller,{
-    updateTabsetPanel(session,"hidden_tabs",selected = paste0("panel",input$controller) )
+output$Tab1 <- renderUI({
+    tabsetPanel(id = "subTabPanel1",
+                tabPanel("sub11"),
+                tabPanel("sub12")
+    )
 })
-
+output$Tab2 <- renderUI({
+    tabsetPanel(id = "subTabPanel2",
+                tabPanel("sub21"),
+                tabPanel("sub22"))
+})
 }
 
 # Run the application
