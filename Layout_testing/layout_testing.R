2cf32de3c258a26917f8b998b55b97dc28588c3b
James <delaneyj@pdx.edu>
2021-11-22 21:17:36 -0800

    This is closer to what I am looking for ... still want the sidebar layout

--- Layout_testing/layout_testing.R
+++ Layout_testing/layout_testing.R
@@ -4,33 +4,45 @@ library(shiny)
 # Define UI for application that draws a histogram
 ui <- fluidPage(
 
-    # Application title
-    titlePanel("Old Faithful Geyser Data"),
+    titlePanel("Mock App"),
 
-        # Show a plot of the generated distribution
-            tabPanel("Test",
-                     tabsetPanel(
-                         tabPanel("First",
-                                  fluidRow)
+    tabsetPanel(
+        tabPanel("Chemistry",
+                 tabsetPanel(
+                     tabPanel("First",
+                              fluidRow(
+                                  column(width = 6,
+                                         plotOutput("Plot1", height = "350px")
+                                  )
+                              )
+                     ),
+                     tabPanel("Second",
+                              fluidRow(
+                                  column(width = 6,
+                                         plotOutput("Plot2", height = "350px")
+                                  )
+                              )
                      )
-
-            )
+                 ) #Close inner tabsetPanel
+        ),
+        tabPanel("Plasma",
+                 fluidRow(
+                     column(width = 12,
+                            h1("HI!")
+                     )
+                 )
         )
+    ) #Close outer tabsetPanel
+) #Close FluidPage
 
+server <- function(input, output) {
 
-# Define server logic required to draw a histogram
-server <- function(input, output, session) {
-output$Tab1 <- renderUI({
-    tabsetPanel(id = "subTabPanel1",
-                tabPanel("sub11"),
-                tabPanel("sub12")
-    )
-})
-output$Tab2 <- renderUI({
-    tabsetPanel(id = "subTabPanel2",
-                tabPanel("sub21"),
-                tabPanel("sub22"))
-})
+    output$Plot1 <- renderPlot({
+        plot(seq(0,10), seq(100, 110), col = "red")
+    })
+    output$Plot2 <- renderPlot({
+        plot(seq(0,10), seq(0, 10)^2, type = "b")
+    })
 }
 
 # Run the application
