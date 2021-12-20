031dcd574e3f10b8bcb38e789bfeb2355960d358
delaneyj1786 <50038148+delaneyj1786@users.noreply.github.com>
2021-12-19 17:18:57 -0800

    I don'tr know what the hell is going on .. still not compliling (removed reenforcinator modules)

--- app3/reinforcinator_modules.R
+++
@@ -1,1 +0,0 @@
-# https://www.ardata.fr/en/post/2019/04/26/share-reactive-among-shiny-modules/
---
+++ modularize_test/module_text_test_script.R
@@ -0,0 +1,44 @@
+#https://mastering-shiny.org/scaling-modules.html#module-ui
+#
+# ui <- fluidPage(
+#   selectInput("var", "Variable", names(mtcars)),
+#   numericInput("bins", "bins", 10, min = 1),
+#   plotOutput("hist")
+# )
+# server <- function(input, output, session) {
+#   data <- reactive(mtcars[[input$var]])
+#   output$hist <- renderPlot({
+#     hist(data(), breaks = input$bins, main = input$var)
+#   }, res = 96)
+# }
+
+require(shiny)
+
+histogramUI <- function(id){
+  taglist(
+    selectInput(NS(id, "var"), "Variable", choices = names(mtcars)),
+    numericInput(NS(id,"bins"), "bins", value = 10, min = 1),
+    plotOutput(NS(id,"hist"))
+  )
+}
+
+histogramServer <- function(id){
+  moduleServer(id, function(input,output,session){
+    data <- reactive(mtcars[[input$var]])
+    output$hist <- renderPlot({
+      hist(data(), breaks = input$bins, main = inpu$var)
+    }, res = 96)
+  })
+}
+
+histogramApp <- function(){
+  ui <- fluidPage(
+    histogramUI("hist1")
+  )
+
+server<- function(input, output, session){
+  histogramServer("hist1")
+}
+
+shinyApp(ui,server)
+}
