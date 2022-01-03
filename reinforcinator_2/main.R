3a167fe2c89d9b7e42f1329e63ef082e4871e64e
delaneyj1786 <50038148+delaneyj1786@users.noreply.github.com>
2022-01-02 17:44:20 -0800

    ho shit - its sort of working (immediate disconnect but still ..)

--- reinforcinator_2/main.R
+++ reinforcinator_2/main.R
@@ -2,7 +2,7 @@ require(shiny)
 require(devtools)
 #library(tidyverse)
 #require(plotly)
-devtools::install_github("delaneyj1786/REINFORCINATOR", force = TRUE)
+#devtools::install_github("delaneyj1786/REINFORCINATOR", force = TRUE)
 require(ReenforcinateR)
 # https://github.com/rstudio/renv/issues/308
 # I TGHINK The solution is up there^^
